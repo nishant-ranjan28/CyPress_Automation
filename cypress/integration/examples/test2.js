cbe9d7a34c996aa02d45008d0fe23c37cb3693a0
n-ishant <nishantranjan78@gmail.com>
2019-11-04 12:32:41 +0530

    test2 created

---
+++ cypress/integration/examples/test2.js
@@ -0,0 +1,23 @@
+/// <reference types="Cypress" />
+
+describe("My Second test suite", function(){
+
+    it("My first second case", function(){
+
+        cy.visit("https://rahulshettyacademy.com/seleniumPractise/#/");
+        cy.get('.search-keyword').type('ca')
+        cy.wait(2000)
+
+        cy.get('.products').as('productLocator')
+
+        cy.get('@productLocator').find('.product').each(($el, index, $list) => {
+            const textVeg = $el.find('h4.product-name').text()
+            if(textVeg.includes('Cashews')){
+                $el.find('button').click()
+            }
+        })
+        cy.get('.cart-icon > img').click()
+        cy.contains('PROCEED TO CHECKOUT').click()
+    })
+
+})
